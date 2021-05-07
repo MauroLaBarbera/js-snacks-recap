02e526b58a9f11bfd03f70afd4afde75e71edf1a
MauroLaBarbera <you@example.com>
2021-05-07 17:18:18 +0200

    snack2

--- js/main.js
+++ js/main.js
@@ -11,3 +11,27 @@ const newArray = array.map((e, i) => {
 });
 
 console.log(newArray);
+
+/**SNACK 2
+Un oggetto rappresenta una finestra di un browser e contiene due campi: 
+un array di tab aperte
+un numero che indica l'indice della tab aperta nell'array:
+{
+    "tab": ["Facebook", "GitHub", "Gmail"],
+    "activeTab": 0
+}
+Il software deve guardare se c'è un social aperto ed eliminarlo dalle tab.
+Nel caso la tab fosse attiva, deve attivare la successiva. */
+
+const browser = {
+    tab: ['Facebook', 'Gmail', 'GitHub', 'Youtube', 'Twitch'],
+    activeTab: 0,
+};
+
+browser.tab.forEach((element, index, array) => {
+    if (index == browser.activeTab) {
+        array.splice(browser.activeTab, 1);
+    }
+});
+
+console.log(browser);
